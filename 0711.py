031b5bd886910f7cb6561ae62e02510e4d4a82ff
Lee Jinsol <imafrog1@gmail1.com>
2014-07-14 19:38:38 +0900

    helloworld

---
+++ 0711.py
@@ -0,0 +1,9 @@
+string="g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj."
+string1=""
+string2=""
+string1=string.replace("k","m")
+string2 =string1.replace("o","q")
+string=string2.replace("e","g")
+print string
+
+print 2**38
